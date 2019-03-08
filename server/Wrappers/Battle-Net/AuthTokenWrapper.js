50a1078d62dc1c48091f7fef3d5672dffdcc2427
Desomph <de.nadlacki@gmail.com>
2019-03-08 09:28:26 +0100

    add authtoken cache, so that we don't request it too often

--- server/Wrappers/Battle-Net/AuthTokenWrapper.js
+++ server/Wrappers/Battle-Net/AuthTokenWrapper.js
@@ -5,11 +5,45 @@ const AuthTokenUrls = require("../../Urls/Battle-Net/AuthToken");
 const config = require("../../../config/config");
 const fetch = require("node-fetch");
 const _ = require("lodash");
+const minute = 60000; // In milis
+
+/**
+ * Keep information of the last time token was requested along with the token itself, so we don't request it too often.
+ */
+var authTokenCache = {
+  token: undefined,
+  lastTimeRequested: undefined
+}
 
 /**
  * Retrieve Battle-Net authentication token.
  */
 getAuthToken = async () => {
+  if (_.isUndefined(authTokenCache.token) || _.isUndefined(authTokenCache.lastTimeRequested)) {
+    console.log(`Requesting new Battle-Net token.`);
+    const token = await retrieveTokenFromBnet();
+    authTokenCache = {
+      token,
+      lastTimeRequested: new Date()
+    }
+  } else {
+    if (authTokenCache.lastTimeRequested < (Date.now() - (minute * 5))) {
+      console.log(`Requesting new Battle-Net token.`);
+      const token = await retrieveTokenFromBnet();
+      authTokenCache = {
+        token,
+        lastTimeRequested: new Date()
+      }
+    }
+  }
+
+  return authTokenCache.token;
+}
+
+/**
+ * Inner method to send the request to Battle-Net API.
+ */
+retrieveTokenFromBnet = async () => {
   const base64token = Buffer.from(config.bnet.clientId + ":" + config.bnet.clientSecret).toString("Base64");
   const authentication = await fetch(AuthTokenUrls.Oauth.EU, {
     body: "grant_type=client_credentials",
@@ -20,7 +54,7 @@ getAuthToken = async () => {
     method: "POST"
   });
 
-  const authToken = await authentication.json();
+  authToken = await authentication.json();
   return authToken.access_token;
 }
 
