2519327517f921a2e02ef30e959a128a42c130b1
Desomph <de.nadlacki@gmail.com>
2019-03-08 08:35:19 +0100

    finish character wrapper

--- server/Wrappers/WoW/CharacterWrapper.js
+++ server/Wrappers/WoW/CharacterWrapper.js
@@ -4,12 +4,32 @@
 const fetch = require("node-fetch");
 const _ = require("lodash");
 const AuthTokenWrapper = require("../Battle-Net/AuthTokenWrapper");
-
-// Model
 const Character = require("../../Model/WoW/Character");
-
-// URL Mappings
 const CharacterUrls = require("../../Urls/WoW/Character");
+
+async function getCharacterEU(realm, name, ...fields) {
+  const authToken = await AuthTokenWrapper.getAuthToken();
+  const initialResponse = await fetch(CharacterUrls.Single("EU", realm, name, ...fields), {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+
+  const response = await initialResponse.json();
+  return new Character(response);
+}
+
+async function getCharacterUS(realm, name, ...fields) {
+  const authToken = await AuthTokenWrapper.getAuthToken();
+  const initialResponse = await fetch(CharacterUrls.Single("US", realm, name, ...fields), {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+
+  const response = await initialResponse.json();
+  return new Character(response);
+}
 
 /**
  * Get character depending on passed in parameters.
@@ -20,22 +40,14 @@ const CharacterUrls = require("../../Urls/WoW/Character");
  * @param  {...any} fields - Rest of the fields, if any.
  */
 async function getCharacter(region, realm, name, ...fields) {
-  switch (region.toLowerCase()) {
-    case 'eu':
+  switch (region) {
+    case 'EU':
       return getCharacterEU(realm, name, ...fields);
-    case 'us':
+    case 'US':
       return getCharacterUS(realm, name, ...fields);
     default:
       throw `Region ${region} does not exist, or is not yet implemented.`;
   }
-}
-
-async function getCharacterEU(realm, name, ...fields) {
-  return new Character("Dejan", 120, "Male", "Undead", "Death Knight", 1, 2, 34);
-}
-
-async function getCharacterUS(realm, name, ...fields) {
-  return new Character("Dejan", 120, "Male", "Undead", "Death Knight", 1, 2, 34);
 }
 
 /**
