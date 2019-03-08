55bf060a18026744c37f8ff534f78229ddeb387b
Desomph <de.nadlacki@gmail.com>
2019-03-08 15:30:28 +0100

    change server requests to start with /api/

--- client/src/App.js
+++ client/src/App.js
@@ -2,85 +2,85 @@ import React, { Component } from 'react';
 import './style/css/App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      tokenEU: null,
-      tokenUS: null,
-      affixes: []
-    }
-  }
+		this.state = {
+			tokenEU: null,
+			tokenUS: null,
+			affixes: []
+		}
+	}
 
-  componentDidMount() {
-    fetch("/eu/token/price")
-      .then(re => re.json())
-      .then(token => {
-        this.setState({
-          tokenEU: token
-        })
-      });
+	componentDidMount() {
+		fetch("/api/eu/token/price")
+			.then(re => re.json())
+			.then(tokenEU => {
+				this.setState({
+					tokenEU
+				});
+			});
 
-    fetch("/us/token/price")
-      .then(re => re.json())
-      .then(token => {
-        this.setState({
-          tokenUS: token
-        })
-      });
+		fetch("/api/us/token/price")
+			.then(re => re.json())
+			.then(tokenUS => {
+				this.setState({
+					tokenUS
+				});
+			});
 
-    fetch("/eu/mythic/affixes")
-      .then(re => re.json())
-      .then(affixes => {
-        this.setState({
-          affixes
-        })
-      });
-  };
+		fetch("/api/eu/mythic/affixes")
+			.then(re => re.json())
+			.then(affixes => {
+				this.setState({
+					affixes
+				});
+			});
+	};
 
-  renderWoWToken(token) {
-    return (
-      <div>
-        <h1>Region: {token.region}</h1>
-        <h2>Price: {(token.price / (100 * 100)).toLocaleString()}g</h2>
-        <h3>Last updated: {token.lastUpdated}</h3>
-      </div>
-    )
-  }
+	renderWoWToken(token) {
+		return (
+			<div>
+				<h1>Region: {token.region}</h1>
+				<h2>Price: {(token.price / (100 * 100)).toLocaleString()}g</h2>
+				<h3>Last updated: {token.lastUpdated}</h3>
+			</div>
+		)
+	}
 
-  renderAffix(affix) {
-    return (
-      <div key={affix.id}>
-        <img src={affix.imageUrl} alt="" />
-        <h3>{affix.name}</h3>
-        <h4>{affix.description}</h4>
-      </div>
-    )
-  }
+	renderAffix(affix) {
+		return (
+			<div key={affix.id}>
+				<img src={affix.imageUrl} alt="" />
+				<h3>{affix.name}</h3>
+				<h4>{affix.description}</h4>
+			</div>
+		)
+	}
 
-  render() {
-    const { tokenEU, tokenUS, affixes } = this.state;
+	render() {
+		const { tokenEU, tokenUS, affixes } = this.state;
 
-    return (
-      <div className="App">
-        <div>
-          {tokenEU && this.renderWoWToken(tokenEU)}
+		return (
+			<div className="App">
+				<div>
+					{tokenEU && this.renderWoWToken(tokenEU)}
+				</div>
+				<div>
+					--------------------------------------
         </div>
-        <div>
-          --------------------------------------
+				<div>
+					{tokenUS && this.renderWoWToken(tokenUS)}
+				</div>
+				<div>
+					--------------------------------------
         </div>
-        <div>
-          {tokenUS && this.renderWoWToken(tokenUS)}
-        </div>
-        <div>
-          --------------------------------------
-        </div>
-        <div>
-          {affixes && affixes.map(this.renderAffix)}
-        </div>
-      </div>
-    );
-  }
+				<div>
+					{affixes && affixes.map(this.renderAffix)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
